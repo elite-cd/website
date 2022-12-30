9963ec7c80bd9d922ea9d73cb8554fc229e0683c
Jonathan Kashongwe <jokashongwe@gmail.com>
2022-12-30 23:44:10 +0100

    Rewording - text en construction

--- src/components/pages/Maintenance/index.js
+++ src/components/pages/Maintenance/index.js
@@ -14,9 +14,7 @@ const MantenancePage = () => {
         />
         <h1 className={style.heading}>Site en construction</h1>
         <div className={style.content}>
-          <p className={style.paragraph}>
-            Désolé le site est en construction, revenez plus tard
-          </p>
+          <p className={style.paragraph}>Revenez plus tard</p>
         </div>
       </div>
       <div className={style.maintenance_gif}>
