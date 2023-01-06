a6dcd85968dd9a0261f1cb75b42858b35d64d65c
Jonathan Kashongwe <jkashongwe_243tech@rhetorik.com>
2023-01-06 16:36:10 +0100

    hotfix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'gatsby';
-import T from 'prop-types';
-import * as React from 'react';
-import ButtonOulined from './button-outlined';
-import ButtonPrimary from './button-primary';
-import ButtonRounded from './button-rounded';
-import * as style from './style.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "gatsby";
+import T from "prop-types";
+import * as React from "react";
+import ButtonOulined from "./button-outlined";
+import ButtonPrimary from "./button-primary";
+import ButtonRounded from "./button-rounded";
+import * as style from "./style.module.scss";
 const Button = ({ text, className, leftIcon, rightIcon }) => {
   return (
-    <button className={style[className ? className : 'button__primary']}>
-      {leftIcon && <FontAwesomeIcon icon={leftIcon} />}
+    <button className={style[className ? className : "button__primary"]}>
+      {leftIcon && <FontAwesomeIcon className={style.icon} icon={leftIcon} />}
       <span className={style.text}>{text}</span>
       {rightIcon && <FontAwesomeIcon icon={rightIcon} />}
     </button>
@@ -18,7 +18,7 @@ const Button = ({ text, className, leftIcon, rightIcon }) => {
 
 const ButtonLink = ({ text, className, leftIcon, rightIcon, url }) => {
   return (
-    <Link to={url} className={style[className ? className : 'button__primary']}>
+    <Link to={url} className={style[className ? className : "button__primary"]}>
       {leftIcon && <FontAwesomeIcon icon={leftIcon} />}
       <span className={style.text}>{text}</span>
       {rightIcon && <FontAwesomeIcon icon={rightIcon} />}
